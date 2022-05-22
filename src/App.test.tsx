fbabbe1737729db093804b543033605770a55a2f
Boris Krasko <boris.krasko@live.com>
2022-05-22 10:47:29 +0200

    feat: add Data component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './Components/Data';
 
 test('renders learn react link', () => {
   render(<App />);
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './Components/Data';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
