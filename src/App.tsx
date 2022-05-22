171fca45393a5d917b7fa9c250b50551c41e023c
Boris Krasko <boris.krasko@live.com>
2022-05-22 10:59:39 +0200

    feat: add Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import Data from "./Components/Data";
 import Input from "./Components/Input";
-
+import Button from "./Components/Button";
+import Data from "./Components/Data";
 
 const App = () => {
   return (
     <>
       <Input value={0} onChange={()=>{}} />
+      <Button children={0} onClick={()=>{}} />
       <Data />
     </>
   );
